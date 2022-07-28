37ade4d46ad55ca33a3514c0fcc44c6df9661aa1
Jake Lindell <jlindell@icr-team.com>
2022-07-28 11:50:02 -0600

    part 1 done

--- Part1.py
+++ Part1.py
@@ -1,13 +1,17 @@
 import json
 import requests
+import operator
 
-with open('data_examples/avg_visit_dur.json', 'r') as f: # companies
-    avg_visit_dur = json.load(f)
+with open('data_examples/pages_per_visit.json', 'r') as f: # companies
+    ppv = json.load(f)
+with open('data_examples/avg_visit_dur.json', 'r') as f: # total_traffic
+    avg_visit_duration = json.load(f)
 with open('data_examples/top_sites_desktop.json', 'r') as f: # total_traffic
     desktop = json.load(f)
 
 
 api_key = ''
+industry = ''
 country = ''
 start_date = ''
 end_date = ''
@@ -15,6 +19,7 @@ main_domain_only = ''
 granularity = ''
 show_verified = ''
 mtd = ''
+get_all = ''
 
 # STEP 1: get all of the companies we could look at
 from data import industry_analysis
@@ -27,73 +32,61 @@ top_desktop_apps = top_desktop_sites['top_sites']
 # lets do an aggregate score, but in order to take into account both, we are going to do a rank of 1 = 100 points, 2 = 9, etc., so if a company
 # is on both at 1, they get 200 points
 
-def rank_score(top_mobile_sites, top_desktop_sites):
+def rank_score(top_mobile_apps, top_desktop_apps):
     agg = {}
 
-    for i in top_mobile_sites:
+    for i in top_mobile_apps:
         if i['domain'] in agg.keys():
-            agg[i['domain']] += (1 + len(top_mobile_sites) - i['rank'])
+            agg[i['domain']] += (1 + len(top_mobile_apps) - i['rank'])
         else:
-            agg[i['domain']] = (1 + len(top_mobile_sites) - i['rank'])
-    for j in top_desktop_sites:
+            agg[i['domain']] = (1 + len(top_mobile_apps) - i['rank'])
+    for j in top_desktop_apps:
         if j['domain'] in agg.keys():
-            agg[j['domain']] += (1 + len(top_desktop_sites) - j['rank'])
+            agg[j['domain']] += (1 + len(top_desktop_apps) - j['rank'])
         else:
-            agg[j['domain']] = (1 + len(top_desktop_sites) - j['rank'])
+            agg[j['domain']] = (1 + len(top_desktop_apps) - j['rank'])
     return agg
 # now we have all of the top apps for both desktop and mobile, and a good way to distinguish their rank
 
 
 ## NOW LETS GET THE TRAFFIC NUMBERS FOR THE TOP X AMOUNT OF COMPANIES
+## put domain names in domains ex. domains = ['bbc.com', 'spotify.com']
 from data import total_traffic
 ### avg_visits_dur
 def get_avg_visits_dur(api_key, domains, country, start_date, end_date, main_domain_only, granularity, show_verified, mtd):
-    data, scores = [], []
+    avg_visits_score = {}
     for i in domains:
-        dur = {}
         avg_visit_duration = total_traffic.avg_visit_dur(api_key, i, country, start_date, end_date, main_domain_only, granularity, show_verified, mtd)
         name = avg_visit_duration['meta']['request']['domain']
-        durs = avg_visit_duration['average_visit_duration']
-        dur[name] = durs
-        data.append(dur)
-    for name in domains:
-        for d in len(data[name]):
-            dom_score = []
-            if name in dom_score.keys():  
-                dom_score[name] += data[name][d]['average_visit_duration']
-            else:
-                dom_score[name] = data[name][0]['average_visit_duration']
-    return dom_score
+        avg_visits_score[name] = sum([x['average_visit_duration'] for x in avg_visit_duration['average_visit_duration']])
+    return avg_visits_score
 
-    
 ### pages per visit
 def pages(api_key, domains, country, start_date, end_date, main_domain_only, granularity, show_verified, mtd):
-    data, scores = [], []
+    ppv_score = {}
     for i in domains:
-        pg = {}
         ppv = total_traffic.pages_per_visit(api_key, i, country, start_date, end_date, main_domain_only, granularity, show_verified, mtd)
         name = ppv['meta']['request']['domain']
-        pgs = ppv['pages_per_visit']
-        pg[name] = pgs
-        data.append(pg)
-    for name in domains:
-        for d in len(data[name]):
-            ppv_score = []
-            if name in ppv_score.keys():  
-                ppv_score[name] += data[name][d]['pages_per_visit']
-            else:
-                ppv_score[name] = data[name][0]['pages_per_visit']
+        ppv_score[name] = sum([x['pages_per_visit'] for x in ppv['pages_per_visit']])
     return ppv_score
 
+
+# run the functions for the top X amount of companies you want, or just input domain names into apps
 app_scores = rank_score(top_mobile_apps, top_desktop_apps)
-apps = app_scores.keys()
+apps = list(app_scores.keys())
 avg_dur = get_avg_visits_dur(api_key, apps, country=country, start_date=start_date, end_date=end_date, 
                             main_domain_only=main_domain_only, granularity=granularity, show_verified=show_verified, mtd=mtd)
 pgs_per_visit = pages(api_key, apps, country=country, start_date=start_date, end_date=end_date, 
                             main_domain_only=main_domain_only, granularity=granularity, show_verified=show_verified, mtd=mtd)
 
 agg_scores = []
+for comp in apps:
+    new_agg_score = {}
+    new_agg_score[comp] = app_scores[comp] + avg_dur[comp] + pgs_per_visit[comp]
 
+
+# this outputs the final score in descending order
+new_agg_score = dict( sorted(new_agg_score.items(), key=operator.itemgetter(1),reverse=True))
 
 
 
