d1137fa49375c1771991025bafec1857f0cdce09
jrlindell <jrlindell@hotmail.com>
2022-07-29 05:58:15 -0600

    fixed name

--- Part2.py
+++ Part2.py
@@ -1,58 +1,98 @@
-#  Please draw out insights around growth, retention, user behavior, concentration, predictive behavior, etc. along with any other things you think are important to point out.
-# growth, retention, user behavior, concenration, predictive behavior
-
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import datetime
 
-data = pd.read_csv('Dataset.csv', nrows=100)
+print('started download')
+data = pd.read_csv('/Users/footb/Downloads/Dataset.csv')
+print('completed download')
+data.head(5).to_csv('outputs/head.csv')
 
-# 
-# order, deposit, prize claimed
-# what is the avg buy in amount?
-
-data.groupby(data['transaction_type'])
-# who wins the most? which state has the most winners?
-
-
-
-## which user participates the most? which states have the most gamblers?
-
-
-
-# value/revenue by user
-data.groupby(['user_id'])['transaction_value'].count() # how many purchases by user
-data.groupby(['user_id'])['transaction_value'].mean() # average transaction value by user
-
-# avg time between transactions per user
-
-# # of transactons, transaction value, transaction revenue by week/month. show growth over time
-
-## transactions per month
+# START WITH EDA
+# what is the user engagement over time?
 data['transaction_timestamp'] = pd.to_datetime(data['transaction_timestamp'])
-data['year'] = pd.DatetimeIndex(data['transaction_timestamp']).year
-data['month'] = pd.DatetimeIndex(data['transaction_timestamp']).month
+data['date'] = data['transaction_timestamp'].dt.date
 
-data.groupby(['year', 'month'])['state'].count() # transactions per month by year
-data.groupby(['month'])['state'].count() # transactions per month by year
-data.groupby(['year', 'month'])['transaction_value'].sum() # spent per month
-data.groupby(['year', 'month'])['transaction_revenue'].sum() # revenue per month
+#month_gb = data.groupby(['year', 'month'])['transaction_revenue'].sum().reset_index(name = 'revenue')
 
-# plot revenue per month
-a = data.groupby(['year', 'month'])['transaction_revenue'].sum()
-a.plot.bar()
+fig, axs = plt.subplots(figsize=(11.5,4))
+data.groupby(['date'])['transaction_revenue'].sum().plot(kind='line')
+plt.xlabel('Date')
+plt.ylabel('Revenue ($)')
+plt.savefig('plots/transaction_revenue_over_time.png')
+
+### zoomed in
+fig, axs = plt.subplots(figsize=(11.5,4))
+a = data[data['date'] >= datetime.date(2021, 1, 1)]
+a.groupby(['date'])['transaction_revenue'].sum().plot(kind='line')
+plt.xlabel('Date')
+plt.ylabel('Revenue ($)')
+plt.savefig('plots/transaction_revenue_over_time_zoomed.png')
 
 
-# how often do users rebuy soomething?
+# do line per transaction type
+#########
+a = data.groupby(['date', 'transaction_type'])['transaction_revenue'].sum().reset_index(name = 'revenue')
+fig, ax = plt.subplots(figsize=(11.5, 4))
+for type in list(a['transaction_type'].unique()):
+    ax.plot(a[a.transaction_type==type].date, a[a.transaction_type==type].revenue,label=type)
+plt.xlabel('Date')
+plt.ylabel('Revenue ($)')
+plt.legend()
+plt.savefig('plots/transaction_revenue_over_time_by_type.png')
 
-len(data[data.groupby(['user_id'])['state'].transform('count') > 1]) / len(data) # X% of users rebought something
+### zoomed
+t = data[data['date'] >= datetime.date(2021, 1, 1)]
+a = t.groupby(['date', 'transaction_type'])['transaction_revenue'].sum().reset_index(name = 'revenue')
+fig, ax = plt.subplots(figsize=(11.5, 4))
+for type in list(a['transaction_type'].unique()):
+    ax.plot(a[a.transaction_type==type].date, a[a.transaction_type==type].revenue,label=type)
+plt.xlabel('Date')
+plt.ylabel('Revenue ($)')
+plt.legend()
+plt.savefig('plots/transaction_revenue_over_time_by_type_zoomed.png')
 
-# state with mos rebuys
+# avg transaction value? revenue? (for both winners and orders)
+a = data.groupby(['date', 'transaction_type'])['transaction_revenue'].sum().reset_index(name = 'revenue')
+a.boxplot(column=['revenue'], by='transaction_type')
+plt.savefig('plots/revenue_box.png')
 
-# do users usually spend small, then big? usually spend the same?
+b = data.groupby(['date', 'transaction_type'])['transaction_value'].sum().reset_index(name = 'value')
+b.boxplot(column=['value'], by='transaction_type')
+plt.savefig('plots/value_box.png')
 
-# what is the breakdown by state? are there states with more/less? better marketing in those states?
-data.groupby(['state'])['transaction_revenue'].count() # number of purchases by state
-data.groupby(['state'])['user_id'].nunique() # number of unique users by state
-data.groupby(['state'])['transaction_value'].mean()
+### by state
+a = data.groupby(['state'], as_index=True)['transaction_revenue'].sum().reset_index(name = 'revenue')
+a.set_index(['state']).plot(kind='bar', xlabel='State', ylabel='Revenue ($)', legend=False)
+plt.savefig('plots/revenue_state.png')
 
-z = 2
+b = data.groupby(['state'], as_index=True)['transaction_value'].sum().reset_index(name = 'value')
+b.set_index(['state']).plot(kind='bar', xlabel='State', ylabel='Value ($)', legend=False)
+plt.savefig('plots/value_state.png')
+
+
+
+# what states are most active?
+a = data.groupby(['state'])['user_id'].count()
+a.to_csv('outputs/state_count.csv')
+a = data.groupby(['state'])['transaction_revenue'].sum()
+a.to_csv('outputs/state_revenue.csv')
+
+
+# what states win the most?
+a = data[data['transaction_type'] == 'Prize claimed'].groupby(['state'])['transaction_revenue'].count()
+a.to_csv('outputs/state_wins.csv')
+
+# X% of users rebought something
+t = len(data[data.groupby(['user_id'])['state'].transform('count') > 1]) / len(data) * 100
+print(t, "% of users re-played")
+
+# growth over time by state
+
+a = data.groupby(['date', 'state'])['transaction_revenue'].sum().reset_index(name = 'revenue')
+fig, ax = plt.subplots()
+for type in list(a['state'].unique()):
+    ax.plot(a[a.state==type].date, a[a.state==type].revenue,label=type)
+plt.xlabel('Date')
+plt.ylabel('Revenue ($)')
+plt.legend()
+plt.savefig('plots/growth_by_state.png')
